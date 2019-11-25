6cf366d24dc55864a317028bd0c11d3ba97ad345
Joe Brew <joebrew@gmail.com>
2019-11-25 23:37:35 +0300

    choices fix

--- app.R
+++ app.R
@@ -1346,7 +1346,7 @@ server <- function(input, output, session) {
       message('chosen flood ', chosen_flood)
       
       no_go <- c()
-      if(length(flood_choices) == 0){
+      if(length(chosen_flood) == 0){
         no_go <- c(no_go, 'Flood')
         flood_go <- br()
       } else {
@@ -1357,7 +1357,7 @@ server <- function(input, output, session) {
                        selected = chosen_flood,
                        inline = TRUE))
       }
-      if(length(earthquake_choices) == 0){
+      if(length(chosen_earthquake) == 0){
         no_go <- c(no_go, 'Earthquake')
         earthquake_go <- br()
       } else {
@@ -1369,7 +1369,7 @@ server <- function(input, output, session) {
                        inline = TRUE)
         )
       }
-      if(length(drought_choices) == 0){
+      if(length(chosen_drought) == 0){
         no_go <- c(no_go, 'Drought')
         drought_go <- br()
       } else {
@@ -1381,7 +1381,7 @@ server <- function(input, output, session) {
                        inline = TRUE)
         )
       }
-      if(length(storm_choices) == 0){
+      if(length(chosen_storm) == 0){
         no_go <- c(no_go, 'Storm')
         storm_go <- br()
       } else {
